bc955a6878b3fd6fc80cde9c15e467844122a2f8
pc9839a-lgtm <pc9839a@naver.com>
2026-08-07 09:49:26 +0900

    Load signup referral field styles

--- src/screens/HomeScreens.jsx
+++ src/screens/HomeScreens.jsx
@@ -3,5 +3,6 @@ import AuthScreen from './AuthScreen.jsx';
 import { CreateLandingModal, StartModeOverlay } from './CreateLandingFlow.jsx';
 import DashboardPolished from './DashboardScreen.jsx';
 import './HomeScreens.css';
+import './AuthReferral.css';
 
 export { PublicHome, AuthScreen, DashboardPolished as Dashboard, CreateLandingModal, StartModeOverlay };
