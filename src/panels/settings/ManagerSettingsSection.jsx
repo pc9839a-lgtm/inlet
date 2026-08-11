2da168310b4a195e211823a7937269cb03942061
pc9839a-lgtm <pc9839a@naver.com>
2026-08-11 21:29:19 +0900

    cleanup: use settings workspace as sole manager UI owner

--- src/panels/settings/ManagerSettingsSection.jsx
+++ src/panels/settings/ManagerSettingsSection.jsx
@@ -1,7 +1,6 @@
 import ManagerList from './ManagerList.jsx';
 import ManagerOwnershipTransfer from './ManagerOwnershipTransfer.jsx';
 import SettingsSection from './SettingsSection.jsx';
-import './ManagerAccess.css';
 
 export default function ManagerSettingsSection({
   addManager,
