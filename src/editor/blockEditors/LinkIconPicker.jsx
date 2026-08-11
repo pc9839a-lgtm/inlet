c7a559afad32b7c4ac4a9219781f163b3cdcf7b6
pc9839a-lgtm <pc9839a@naver.com>
2026-08-11 16:53:59 +0900

    refactor: move link media styling to links editor owner

--- src/editor/blockEditors/LinkIconPicker.jsx
+++ src/editor/blockEditors/LinkIconPicker.jsx
@@ -1,13 +1,12 @@
 import LinkEmojiPicker from './LinkEmojiPicker.jsx';
 import LinkIconModeButtons from './LinkIconModeButtons.jsx';
 import LinkThumbPicker from './LinkThumbPicker.jsx';
-import './LinkIconPicker.css';
 
 export function LinkIconPicker({ item, onChange }) {
   const mode = item.iconMode || 'emoji';
 
   return (
-    <div className="link-icon-simple-v2" data-icon-mode={mode}>
+    <div className="link-icon-control-v3" data-icon-mode={mode}>
       <LinkIconModeButtons mode={mode} onChange={onChange} />
       {mode === 'emoji' && <LinkEmojiPicker item={item} onChange={onChange} />}
       {mode === 'thumb' && <LinkThumbPicker item={item} onChange={onChange} />}
