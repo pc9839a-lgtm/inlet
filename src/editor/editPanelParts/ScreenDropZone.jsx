fbd742173ffef739f19882845aa37e9ae9c9f672
pc9839a-lgtm <pc9839a@naver.com>
2026-08-12 09:39:24 +0900

    fix: isolate screen order drop zones

--- src/editor/editPanelParts/ScreenDropZone.jsx
+++ src/editor/editPanelParts/ScreenDropZone.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 export function ScreenDropZone({ last = false, onDragOver, onDragEnter, onDragLeave, onDrop }) {
   return (
     <div
-      className={`screen-drop-zone${last ? ' last' : ''}`}
+      className={`screen-order-v2-drop-zone${last ? ' last' : ''}`}
       onDragOver={onDragOver}
       onDragEnter={onDragEnter}
       onDragLeave={onDragLeave}
       onDrop={onDrop}
+      aria-hidden="true"
     >
       <span />
     </div>
