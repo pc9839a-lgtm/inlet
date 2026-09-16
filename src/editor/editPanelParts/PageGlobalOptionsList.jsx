f6989929136bfcb79a90e2671de89c318b9da8a5
pc9839a-lgtm <pc9839a@naver.com>
2026-09-16 13:00:22 +0900

    refactor(pagero): keep page options global-only

--- src/editor/editPanelParts/PageGlobalOptionsList.jsx
+++ src/editor/editPanelParts/PageGlobalOptionsList.jsx
@@ -1,40 +1,12 @@
 import React from 'react';
 import { AnimationOptionsCard } from './AnimationOptionsCard.jsx';
-import { GlobalFixedBlocks } from './GlobalFixedBlocks.jsx';
 import { ShareOptionsCard } from './ShareOptionsCard.jsx';
 
-export function PageGlobalOptionsList({
-  page,
-  topNavBlock,
-  bottomBlock,
-  footerBlock,
-  hideTopNavControl,
-  openId,
-  updateTheme,
-  updatePage,
-  toggleVisible,
-  toggleBlockOpen,
-  renderTopNavEditor,
-  renderBottomBarEditor,
-  renderFooterEditor,
-}) {
+export function PageGlobalOptionsList({ page, updateTheme, updatePage }) {
   return (
     <div className="page-global-options-list page-global-grid">
       <AnimationOptionsCard page={page} updateTheme={updateTheme} />
       <ShareOptionsCard page={page} updatePage={updatePage} />
-
-      <GlobalFixedBlocks
-        topNavBlock={topNavBlock}
-        bottomBlock={bottomBlock}
-        footerBlock={footerBlock}
-        hideTopNavControl={hideTopNavControl}
-        openId={openId}
-        toggleVisible={toggleVisible}
-        toggleBlockOpen={toggleBlockOpen}
-        renderTopNavEditor={renderTopNavEditor}
-        renderBottomBarEditor={renderBottomBarEditor}
-        renderFooterEditor={renderFooterEditor}
-      />
     </div>
   );
 }
