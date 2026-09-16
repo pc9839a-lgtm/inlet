553c57d02812f184c98f1b74c9a5d3819dccbaa3
pc9839a-lgtm <pc9839a@naver.com>
2026-09-16 13:00:29 +0900

    refactor(pagero): trim page options component contract

--- src/editor/editPanelParts/PageGlobalOptions.jsx
+++ src/editor/editPanelParts/PageGlobalOptions.jsx
@@ -3,38 +3,14 @@ import './PageGlobalOptions.css';
 import { PageGlobalOptionsHeader } from './PageGlobalOptionsHeader.jsx';
 import { PageGlobalOptionsList } from './PageGlobalOptionsList.jsx';
 
-export function PageGlobalOptions({
-  page,
-  topNavBlock,
-  bottomBlock,
-  footerBlock,
-  hideTopNavControl,
-  openId,
-  updateTheme,
-  updatePage,
-  toggleVisible,
-  toggleBlockOpen,
-  renderTopNavEditor,
-  renderBottomBarEditor,
-  renderFooterEditor,
-}) {
+export function PageGlobalOptions({ page, updateTheme, updatePage }) {
   return (
     <section className="card page-global-options-card">
       <PageGlobalOptionsHeader />
       <PageGlobalOptionsList
         page={page}
-        topNavBlock={topNavBlock}
-        bottomBlock={bottomBlock}
-        footerBlock={footerBlock}
-        hideTopNavControl={hideTopNavControl}
-        openId={openId}
         updateTheme={updateTheme}
         updatePage={updatePage}
-        toggleVisible={toggleVisible}
-        toggleBlockOpen={toggleBlockOpen}
-        renderTopNavEditor={renderTopNavEditor}
-        renderBottomBarEditor={renderBottomBarEditor}
-        renderFooterEditor={renderFooterEditor}
       />
     </section>
   );
