a0586dd739bb8a508602b1370f4187f1ddb98c2b
pc9839a-lgtm <pc9839a@naver.com>
2026-09-26 14:02:31 +0900

    fix(pagero): forward inline selected settings

--- src/editor/editPanelParts/ScreenOrderList.jsx
+++ src/editor/editPanelParts/ScreenOrderList.jsx
@@ -3,11 +3,11 @@ import './ScreenOrder.css';
 import { ScreenOrderListHeader } from './ScreenOrderListHeader.jsx';
 import { ScreenOrderListItems } from './ScreenOrderListItems.jsx';
 
-export function ScreenOrderList(props) {
+export function ScreenOrderList({ selectedBlockSettingsProps = null, ...props }) {
   return (
     <section className="screen-order-v2-card">
       <ScreenOrderListHeader />
-      <ScreenOrderListItems {...props} />
+      <ScreenOrderListItems {...props} selectedBlockSettingsProps={selectedBlockSettingsProps} />
     </section>
   );
 }
